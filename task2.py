940f0a39fef1633dcb98f8974191beaa634bdd30
adoroannegare <alinaahmetvalieva55@gmail.com>
2026-09-22 21:43:57 +0400

    Решение задачи Palindrome Number

---
+++ task2.py
@@ -0,0 +1,7 @@
+#Ссылка: https://leetcode.com/problems/palindrome-number/
+class Solution(object):
+    def isPalindrome(self, x):
+        if str(x)[::-1]==str(x):
+            return True
+        else:
+            return False
